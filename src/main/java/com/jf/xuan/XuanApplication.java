c47e1bd6db0ee9e04f23942562de1dfbd1de6981
jf-laptop <jiajinyuan918@163.com>
2019-03-13 14:11:18 +0800

    start successful

--- src/main/java/com/jf/xuan/XuanApplication.java
+++ src/main/java/com/jf/xuan/XuanApplication.java
@@ -1,5 +1,7 @@
 package com.jf.xuan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -18,7 +20,16 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableCaching
 public class XuanApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(XuanApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(XuanApplication.class, args);
+        String sb = "\n   _____ _             _                                      __       _ \n" +
+                "  / ____| |           | |                                    / _|     | |\n" +
+                " | (___ | |_ __ _ _ __| |_   ___ _   _  ___ ___ ___  ___ ___| |_ _   _| |\n" +
+                "  \\___ \\| __/ _` | '__| __| / __| | | |/ __/ __/ _ \\/ __/ __|  _| | | | |\n" +
+                "  ____) | || (_| | |  | |_  \\__ \\ |_| | (_| (_|  __/\\__ \\__ \\ | | |_| | |\n" +
+                " |_____/ \\__\\__,_|_|   \\__| |___/\\__,_|\\___\\___\\___||___/___/_|  \\__,_|_|\n";
+        LOG.info(sb);
     }
 }
