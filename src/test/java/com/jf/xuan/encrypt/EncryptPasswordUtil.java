463678642f6abe4c6b7d05da0601ce565056a910
junfeng <jiajinyuan918@163.com>
2021-01-07 16:31:12 +0800

    升级版本修改

--- src/test/java/com/jf/xuan/encrypt/EncryptPasswordUtil.java
+++ src/test/java/com/jf/xuan/encrypt/EncryptPasswordUtil.java
@@ -1,18 +1,15 @@
 package com.jf.xuan.encrypt;
 
 import org.jasypt.encryption.StringEncryptor;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * 数据库密码加密工具，生成密文
  *
  * @author Junfeng
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class EncryptPasswordUtil {
 
